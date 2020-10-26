7e877f4692beb87c422d24b195b9b68dd54bc392
Ali Sinan Saglam <asinansaglam@gmail.com>
2020-10-26 11:35:13 -0400

    Changed export to match latest dev-MG branch

--- PhysiCell/custom_modules/internal_viral_dynamics.cpp
+++ PhysiCell/custom_modules/internal_viral_dynamics.cpp
@@ -19,11 +19,14 @@ void internal_virus_model_setup( void )
 		// what microenvironment variables do I need? 
 
 		// what custom data do I need? 
+	internal_viral_dynamics_info.microenvironment_variables.push_back( "assembled virion" ); 	
+
 	internal_viral_dynamics_info.cell_variables.push_back( "virion" ); // adhered, in process of endocytosis 
 	internal_viral_dynamics_info.cell_variables.push_back( "uncoated_virion" ); 
 	internal_viral_dynamics_info.cell_variables.push_back( "viral_RNA" ); 
 	internal_viral_dynamics_info.cell_variables.push_back( "viral_protein" ); 
 	internal_viral_dynamics_info.cell_variables.push_back( "assembled_virion" ); 
+	internal_viral_dynamics_info.cell_variables.push_back( "export_virion" ); 
 
 	internal_viral_dynamics_info.cell_variables.push_back( "virion_uncoating_rate" ); 
 	internal_viral_dynamics_info.cell_variables.push_back( "uncoated_to_RNA_rate" ); 
@@ -78,10 +81,6 @@ void internal_virus_model( Cell* pCell, Phenotype& phenotype, double dt )
 	// this transfer is now handled in receptor dynamics 
 */		
 
-  /*
-  pCell->custom_data[nA_internal] = newly assembled virion 
-  phenotype.molecular.internalized_total_substrates[nA_external] = all assembled virion that is not yet exported 
-  */
   // This resets the internal assembled virion count 
   // so we are commenting it out 
 	// pCell->custom_data[nA_internal] = 
@@ -122,35 +121,27 @@ void internal_virus_model( Cell* pCell, Phenotype& phenotype, double dt )
 	pCell->custom_data[nA_internal] += dA; 
 	
 	// set export rate 
-  double nA_export_rate = dt * pCell->custom_data["virion_export_rate"] * pCell->custom_data[nA_internal];
-  // next step is to get a discrete number here
-  // floor + a probability of exporting an additional one
-  double nA_export_count = floor( nA_export_rate );
-  // TODO: Add the stochastic additional one here to nA_export_count
-  // when doing so make sure the internal nA count doesn't drop below
-  // zero by comparing the two
-  // this is the export count to add/remove
-  pCell->custom_data[nA_internal] -= nA_export_count;
-  phenotype.molecular.internalized_total_substrates[nA_external] += nA_export_count;
-//6. adjust phenotype.secretion.net_export_rates[nA_external] so taht dt * export_rate * phenotype.molecular.internalized_total_substrates[nA_external] = number of particles you wanted to export 
-// dt * exp_rate * internal = part_to_exp
-  // is this correct at all? 
-  phenotype.secretion.net_export_rates[nA_external] = nA_export_count/dt;
-	
-	// phenotype.secretion.net_export_rates[nA_external] = 
-	// 	pCell->custom_data["virion_export_rate" ] * pCell->custom_data[nA_internal]; 
+/*	
+	phenotype.secretion.net_export_rates[nA_external] = 
+		pCell->custom_data["virion_export_rate" ] * pCell->custom_data[nA_internal]; 
  
 	// copy data from custom variables to "internalized variables" 
-	
-/*	
+		
 	phenotype.molecular.internalized_total_substrates[nV_external] = 
-		pCell->custom_data[nV_internal];
-*/		
-
-  /* Handled above now
+		pCell->custom_data[nV_internal];		
 	phenotype.molecular.internalized_total_substrates[nA_external] = 
 		pCell->custom_data[nA_internal];	
-  */
+*/			
+	double deP = dt * pCell->custom_data["virion_export_rate" ] * pCell->custom_data[nA_internal]; 
+	if( deP > pCell->custom_data[nA_internal] )
+	{ deP = pCell->custom_data[nA_internal]; } 
+	pCell->custom_data[eP] += deP; 
+	pCell->custom_data[nA_internal] -= deP; 
+	//test int export
+	
+	double alpha1 = floor(pCell->custom_data[eP]);
+	pCell->nearest_density_vector()[nA_external] += alpha1 / microenvironment.mesh.dV; 
+	pCell->custom_data[eP] -= alpha1; 
 	
 	return; 
 }
